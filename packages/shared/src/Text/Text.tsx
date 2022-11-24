bb13ab612dba98661d84199211bec81119c41baa
unknown <nurshahadahrashid@gmail.com>
2022-11-24 23:30:13 +0800

    feat: added anothe feature fro this

--- packages/shared/src/Text/Text.tsx
+++ packages/shared/src/Text/Text.tsx
@@ -1,11 +1,11 @@
 import React, { FunctionComponent } from 'react';
 
 type Props = {
-  children: React.ReactNode,
+  children: React.ReactNode;
 };
 
 const Text: FunctionComponent<Props> = ({ children }) => {
-  console.log("test s")
+  console.log('test swe');
   return <h1>{children}</h1>;
 };
 
